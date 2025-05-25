12d05709f15334ba0162fff81b8de8810bbf75f1
Dujet <duje0807@gmail.com>
2025-05-25 23:39:54 +0200

    change config target

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: 'https://fitness-studio-infsus.onrender.com/',
         changeOrigin: true,
         secure: false
       }
